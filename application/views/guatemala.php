cb9022bc00043392dcce46445b7904dae01a7e89
gonzalbi <gonza-martin@hotmail.com>
2021-03-28 23:37:45 -0300

    style fix and photos update

--- application/views/guatemala.php
+++ application/views/guatemala.php
@@ -1,77 +1,85 @@
-    <!-- ======= Portfolio Section ======= -->
-    <section id="guatemala" class="guatemala section-bg">
+<!-- ======= Portfolio Section ======= -->
+<section id="guatemala" class="guatemala section-bg">
     	<div class="container">
             <div class="row">
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left" style="background-image: url(assets/img/rnrteam1.png);">
+                        <div class="bio">
+                        <div class="title">Fede
+                        ahunchain</div>
+                        <div class="intro-text">
+                            <p>Director at ROCKNROLLA. Trabajó para marcas como Peugeot, Volkswagen y Nissan; Pepsi, Adrenaline Rush, AMP 365, Petit, Salutaris y Grapette; Axe, Magia Blanca y Henkel. Banco G&T Continental y BANTRAB; entre muchas otras marcas y categorías en sus más de 20 años de carrera. Director General y guionista de las exitosas y multipremiadas series animadas de youtube Arturo y Los Frumojis y La Chiripa F.C.
+                            CCO Global de RR GROUP.</p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+                            
+                        </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left" style="background-image: url(assets/img/rnrteam2.png);">
+                        <div class="bio">
+                        <div class="title">Diego Castillo</div>
+                        <div class="intro-text">
+                            <p>Director at ROCKNROLLA. 
+                            Trabajó para marcas como  AMP365, Banco G&T Continental, Seguros G&T, Café Barista, Bayer, Salutaris, Grapette, Yamaha, Pollo Campero, Pollo Granjero, Tortrix, Nissan y Magia Blanca entre muchas otras en sus más de 10 años de carrera.</p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+                    </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left" style="background-image: url(assets/img/rnrteam3.png);">
+                        <div class="bio">
+                        <div class="title">Santi Dobrich</div>
+                        <div class="intro-text">
+                            <p>Director at RocknRolla
+                            Trabajó para marcas como Nestlé, CocaCola, Shell, Volkswagen, Banco G&T, PepsiCo, Pollo Campero, Beiersdorf, Johnson&Johnson, Bantrab, entre muchas otras marcas y categorías, en sus más de 15 años de carrera. Director Integral, copywriter, fanático de las ideas diferentes, consciente de la responsabilidad social de los comunicadores.</p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+                    </div>
                     </div>
                 </div>
             </div>
             <div class="row">
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left" style="background-image: url(assets/img/rnrteam5.png);">
+                        <div class="bio">
+                        <div class="title">Gato Ortiz Molina</div>
+                        <div class="intro-text">
+                            <p>Director de diseño ROCKNROLLA. 
+                                Trabajó para marcas como  Volkswagen, Claro, Goodyear, Cementos Progreso, PepsiCo, Bayer, Zurich Seguros, Cerveza Paeña, Café Barista, Fundación Ayuvi, Adrenaline Rush
+                            a lo largo de 15 años de trayectoria.</p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+                    </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left" style="background-image: url(assets/img/rnrteam4.png);">
+                        <div class="bio">
+                        <div class="title">Leo Cornejo</div>
+                        <div class="intro-text">
+                            <p>DDirector audiovisual at ROCKNROLLA. 
+                            Trabajó para marcas como Cerveza Brahva, Grapette, Pepsi, AMP 365, Tortrix, Lays, Pollo campero, Toledo, Wendy’s, P&G, Volkswagen, Nissan, Banco G&T, Bantrab, Adrenaline Rush, Lipton, Aqua, Salutaris, Café Barista, Bayer, Magia blanca y Néctares Petit entre muchas otras en sus más de 25 años de carrera. </p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+                    </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-3">
-                    <div class="height35 projectContainer centerProject left">
-                        <a href="entreTacTac">
-                        <div class="projectName">
-                            TAC TAC<br>
-                            BRANDED MUSIC VIDEO
+                <div class="col-12 col-md-3 partner">
+                    <div class="heightauto projectContainer centerProject left">
+
+                        <div class="title">Rockandrolla Guatemala City</div>
+                        <div class="intro-text">
+                            <p>hola@wearerocknrolla.com
+                                +502 2261.8932 
+                                Diagonal 6 12-42, Zona 10.
+                                Edificio Design Center
+                                Torre 2, Oficina 1003
+                            Guatemala, Centro América</p>
                         </div>
-                        <img class="project" src="assets/img/portfolio/ent4-tactac.png" />
-                        </a>
+
                     </div>
                 </div>
-            </div>
             </div>
         </div>
     </div>
-    </section><!-- End Portfolio Section -->
+</div>
+</section>
+<!-- End Portfolio Section -->
