89918b3ed791a617c424d138a90a066249753937
rossistefanoseeds <82181932+rossistefanoseeds@users.noreply.github.com>
2021-04-11 11:59:18 -0300

    Update ideasConversation.php

--- application/views/proyectos/ideasConversation.php
+++ application/views/proyectos/ideasConversation.php
@@ -18,74 +18,130 @@
    <section id="ideas" class="ideas section-bg inner">
     <div class="container-fluid">
       <div class="row">
-          <div class="height35 d-block d-md-none col-12 projectDescription">
-                                        <div class="title">be
-a part of
+          <div class="height33 d-block d-md-none col-12 projectDescription" style="overflow:hidden;">
+                                        <div class="title">be<br>
+    <s>a part of</s><br>
 the conversation.</div>
                     <div class="intro-text">
                         <p> 
 En las fechas especiales cuando todas las marcas competimos por la misma atención es cuando más  importante es no ser parte de la conversación y conectar de una forma única e inaesperada.</p>
                     </div>
           </div>
-          <div class="col-12 col-md-6  p-0">
-            <div class="height35 projectContainer col-12 left ">
-                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner" src="assets/img/portfolio/conversation/conversation1.png" />
+
+<div class="height33 projectContainer d-block d-md-none col-12">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation1.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none;">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/WcPJF5dwCK4?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>
+
+<div class="height33 projectContainer d-block d-md-none col-12 left">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation2.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/5qzRCoWTSrE?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>      
-            <div class="height30 projectContainer col-12 left">
-                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner fixCentradoVertx2" src="assets/img/portfolio/conversation/conversation3.png" />
+
+<div class="height33 projectContainer d-block d-md-none col-12">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation3.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/sBnv6keiYf0?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>
-            <div class="height35 projectContainer col-12 left ">
-                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner" src="assets/img/portfolio/conversation/conversation4.png" />
+
+<div class="height33 projectContainer d-block d-md-none col-12">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation4.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/cWp3frR1gKk?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>
+
+
+<div class="height33 projectContainer d-block d-md-none col-12">
+                                         <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+            <img class="projectInner" src="assets/img/portfolio/conversation/conversation5.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&amp;autoplay=1&amp;mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 d-none d-md-block col-md-6  p-0">
+            <div class="height33 projectContainer col-12 left ">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation1.png">
+            </div>
+            <div class="youtube-video-container" style="display:none;">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/WcPJF5dwCK4?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>      
+            <div class="height33 projectContainer col-12 left">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation3.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/sBnv6keiYf0?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>
+            <div class="height33 projectContainer col-12 left ">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation4.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/cWp3frR1gKk?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>
         </div>
-        <div class="col-12 col-md-6 p-0">    
-            <div class="height35 projectContainer col-12 left">
-                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner fixCentradoVertx2" src="assets/img/portfolio/conversation/conversation2.png" />
+        <div class="col-12 d-none d-md-block col-md-6 p-0">    
+            <div class="height33 projectContainer d-none d-md-block col-12 left">
+                                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/conversation/conversation2.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/5qzRCoWTSrE?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>
-          <div class="projectContainer height30 d-none d-md-block col-12 left projectDescription">
-                                        <div class="title">be
-a part of
+          <div class="projectContainer height33 d-none d-md-block col-12 left projectDescription">
+                                        <div class="title" style="margin-top:5%;">be<br>
+    <s>a part of</s><br>
 the conversation.</div>
-                    <div class="intro-text">
+                    <div class="intro-text" style="height:auto;">
                         <p> 
 En las fechas especiales cuando todas las marcas competimos por la misma atención es cuando más  importante es no ser parte de la conversación y conectar de una forma única e inaesperada.</p>
                     </div>
           </div>
-          <div class="height35 projectContainer col-12 left">
-                                         <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-            <img class="projectInner" src="assets/img/portfolio/conversation/conversation5.png" />
+          <div class="height33 projectContainer col-12 left">
+                                         <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+            <img class="projectInner" src="assets/img/portfolio/conversation/conversation5.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&amp;autoplay=1&amp;mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>
