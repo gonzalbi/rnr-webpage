c2cd2cf91bf6d642983a8b7aff402c16e4512f70
rossistefanoseeds <82181932+rossistefanoseeds@users.noreply.github.com>
2021-04-10 23:01:23 -0300

    Update brandingAMP.php

--- application/views/proyectos/brandingAMP.php
+++ application/views/proyectos/brandingAMP.php
@@ -19,59 +19,71 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-xs-12">
-          <div class="height35 d-block d-md-none col-12 projectDescription">
-           <div class="title">AMP
-                    Full branding
+          <div class="height35 d-block d-md-none col-12 projectDescription" style="overflow:hidden;">
+           <div class="title">AMP<br>
+                    Full branding<br>
                     project. </div>
                     <div class="intro-text">
                         <p>AMP 365 es el daily energy de PepsiCo que hemos apoyado a desarrollar como marca en todos los mercados donde se consume en el mundo. Reconfiguración de imagen de etiquetas, generación de extensiones de línea y empaque, talleres de innovación y consistente brand building mediante la comunicación de la marca asociada a estrellas globales que celebran a los trabajadores del día a día.</p>
                     </div>
           </div>
 
-          <div class="height30 projectContainer col-12 col-md-5 left">
-            <div class="centerProject">
-              <img class="projectInner" src="assets/img/portfolio/AMP/AMP1.png" />
+          <div class="height35 projectContainer marTop35 d-block d-md-none col-12">
+            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" style="height:100%;">  
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP3.png" />
             </div>
-          </div>
-          <div class="height30 projectContainer col-12 col-md-5 left">
-            <div class="centerProject">
-              <img class="projectInner" src="assets/img/portfolio/AMP/AMP2.png" />
-            </div>
-          </div>
-          <div class="height65 projectContainer col-12 col-md-2 left ">
-            <img class="projectInner fixTrampita" src="assets/img/portfolio/AMP/AMP6.png" />
-          </div>
-          <div class="height35 projectContainer marTop35 col-12 col-md-6 left">
-            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-            <img class="projectInner fixCentradoVert" src="assets/img/portfolio/AMP/AMP3.png" />
-            </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/xBnbiQrYPs0?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+          </div><div class="height30 projectContainer col-12 col-md-5 left">
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP1.png" /><div class="centerProject">
+              
+            </div>
+          </div>
+          <div class="height30 projectContainer col-12 col-md-5 left">
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP2.png" /><div class="centerProject">
+              
+            </div>
+          </div>
+          <div class="height65 projectContainer d-none d-md-block col-md-2 left ">
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP6.png" />
+          </div>
+          <div class="height35 projectContainer marTop35 d-none d-md-block col-md-6 left">
+            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" style="height: 100%; display: none;">  
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP3.png" />
+            </div>
+            <div class="youtube-video-container" style="display: block;">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/xBnbiQrYPs0?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>
-          <div class="projectContainer marTop30 height30 d-none d-md-block col-md-12 col-6 left projectDescription">
-            <div class="title">AMP
-                    Full branding
+          <div class="projectContainer marTop35 height35 d-none d-md-block col-md-4 col-6 left projectDescription">
+            <div class="title" style="margin-top:5%;">AMP<br>
+                    Full branding<br>
                     project. </div>
-                    <div class="intro-text">
+                    <div class="intro-text" style="height:50%;">
                         <p>AMP 365 es el daily energy de PepsiCo que hemos apoyado a desarrollar como marca en todos los mercados donde se consume en el mundo. Reconfiguración de imagen de etiquetas, generación de extensiones de línea y empaque, talleres de innovación y consistente brand building mediante la comunicación de la marca asociada a estrellas globales que celebran a los trabajadores del día a día.</p>
                     </div>
           </div>
-          <div class="height35 projectContainer col-12 col-md-4 left">
+          <div class="height35 projectContainer d-none d-md-block col-md-4 left">
             <img class="projectInner" src="assets/img/portfolio/AMP/AMP4.png" />
           </div>
-          <div class="height35 projectContainer col-12 col-md-2 left">
+          <div class="height35 projectContainer col-6 col-md-2 left">
             <img class="projectInner" src="assets/img/portfolio/AMP/AMP5.png" />
           </div>
-          <div class="height35 projectContainer col-12 left">
-            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-            <img class="projectInner fixCentradoVert" src="assets/img/portfolio/AMP/AMP3.png" />
+<div class="height35 projectContainer d-block d-md-none col-6 right">
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP10.png" />
+          </div>
+          <div class="height35 projectContainer col-12 col-md-6 left">
+            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" style="height: 100%; display: none;">  
+            <img class="projectInner" src="assets/img/portfolio/AMP/AMP3.png" />
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display: block;">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/jswiRdJlRwU?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>
@@ -82,7 +94,7 @@
   </section><!-- End Portfolio Section -->
 
 
-</main><!-- End #main -->
+</main>
 
 
 <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
