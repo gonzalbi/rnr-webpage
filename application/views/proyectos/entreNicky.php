96b187b5da45180a84136de9bc8a01fe85e00264
rossistefanoseeds <82181932+rossistefanoseeds@users.noreply.github.com>
2021-04-11 17:49:53 -0300

    Update entreNicky.php

--- application/views/proyectos/entreNicky.php
+++ application/views/proyectos/entreNicky.php
@@ -19,52 +19,65 @@
    <section id="ideas" class="ideas section-bg inner">
     <div class="container-fluid">
       <div class="row">
-        <div class="height35 d-block d-md-none col-12 projectDescription">
-           <div class="title">más de mil manos
-branded short film. </div>
+        <div class="height35 d-block d-md-none col-12 projectDescription" style="overflow:hidden;">
+           <div class="title">Nicky Jam<br>music<br>branded video </div>
                     <div class="intro-text">
                         <p>
 Más que un mini documental, el primer documento audiovisual que resume el proceso artesanal que hay detrás del café de Guatemala, uno de los mejores del mundo. Café Barista celebra este arte hecho a mano y junta por primera vez a las más de mil manos que participan de este proceso único en una misma mesa a disfrutar una taza de café y una conversación tan postergada como amena e interesante.</p>
                     </div>
         </div>
-        <div class="col-12 p-0">
-          <div class="projectContainer height50 d-none d-md-block col-md-3 left projectDescription">
-             <div class="title">más de mil manos
-branded short film. </div>
+        <div class="col-md-12 d-none d-md-block p-0">
+          <div class="projectContainer height50 d-none d-md-block col-md-3 left projectDescription" style="padding: 5vh 5vh 5vh 5vh;">
+             <div class="title">Nicky Jam<br>branded<br>music video</div>
                     <div class="intro-text">
-                        <p>
-Más que un mini documental, el primer documento audiovisual que resume el proceso artesanal que hay detrás del café de Guatemala, uno de los mejores del mundo. Café Barista celebra este arte hecho a mano y junta por primera vez a las más de mil manos que participan de este proceso único en una misma mesa a disfrutar una taza de café y una conversación tan postergada como amena e interesante.</p>
+                        <p>Un video clip colaborativo entre la marca de daily energy de PepsiCo y Nicky Jam. Motivación para los trabajadores que se esfuerzan en busca de sus sueños.
+</p>
                     </div>
           </div> 
           <div class="height50 projectContainer col-12 col-md-9 left ">
-                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-            <img class="projectInner fixCentradoVertx2" src="assets/img/portfolio/nicky/nicky5.png" />
+                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+            <img class="projectInner" src="assets/img/portfolio/nicky/nicky5.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/r3myTFe1GR4?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>     
         </div>
-        <div class="col-12 p-0">   
+        <div class="col-md-12 d-none d-md-block p-0">   
           <div class="height25 projectContainer col-12  col-md-4 left">
-            <img class="projectInner " src="assets/img/portfolio/nicky/nicky2.png" />
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky1.png">
           </div>
           <div class="height25 projectContainer col-12  col-md-4 left">
-            <img class="projectInner " src="assets/img/portfolio/nicky/nicky3.png" />
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky2.png">
           </div>
           <div class="height50 projectContainer col-12 col-md-4 left">
-            <img class="projectInner " src="assets/img/portfolio/nicky/nicky6.png" />
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky6.png">
           </div>
           <div class="height25 marTop25 projectContainer col-12  col-md-4 left">
-            <img class="projectInner " src="assets/img/portfolio/nicky/nicky4.png" />
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky3.png">
           </div>
           <div class="height25 marTop25 projectContainer col-12  col-md-4 left">
-            <img class="projectInner " src="assets/img/portfolio/nicky/nicky1.png" />
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky4.png">
           </div>
 
         </div>
+
+<div class="height50 projectContainer d-block d-md-none col-12 col-md-9">
+                           <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+            <img class="projectInner" src="assets/img/portfolio/nicky/nicky5.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/r3myTFe1GR4?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+          </div>
+
+<div class="height25 projectContainer d-block d-md-none col-12 ">
+            <img class="projectInner " src="assets/img/portfolio/nicky/nicky10.png">
+          </div>
       </div>
     </div>
   </section><!-- End Portfolio Section -->
