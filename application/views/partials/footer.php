0514ad68e71c3f7c67d31f5ba437dead0c0ff3e4
gonzalbi <gonza-martin@hotmail.com>
2021-04-11 01:02:09 -0300

    footer imgs

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -1,9 +1,16 @@
 <div id="footer">
     <div class="separator"></div>
     <div class="container-fluid">
-        <div class="row">
+        <div class="row" style="display: inline-flex;">
             <div class="col-xs-12">
                 <p class="footer-text"><small>©</small> <span style="font-size: 1.5vmin;">2020</span> ROCKNROLLA. ALL RIGHTS RESERVED.</p>
+            </div>
+            <div class='footer-img-container'>
+                <img src="assets/img/Recurso 13.png">
+                <img src='assets/img/Recurso 14.png'>
+                <img src='assets/img/Recurso 15.png'>
+                <span style="white-space: nowrap;margin-right: 1vh;font-size: 1.6vmin;">WE ARE AN INNOVATION DRIVEN HOLDING SUPPORTED BY</span>
+                <img src='assets/img/Recurso 16.png' style="width: 10vh;">
             </div>
         </div>
     </div>
