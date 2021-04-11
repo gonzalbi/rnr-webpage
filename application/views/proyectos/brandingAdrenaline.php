a046a9d46cedc7b34dbc083616eb67df343d4e01
rossistefanoseeds <82181932+rossistefanoseeds@users.noreply.github.com>
2021-04-11 00:11:48 -0300

    Update brandingAdrenaline.php

--- application/views/proyectos/brandingAdrenaline.php
+++ application/views/proyectos/brandingAdrenaline.php
@@ -18,9 +18,9 @@
    <section id="ideas" class="ideas section-bg inner">
     <div class="container-fluid">
       <div class="row">
-          <div class="height35 d-block d-md-none col-12 projectDescription">
-                                <div class="title">Adrenaline rush
-                    Full branding
+          <div class="height35 d-block d-md-none col-12 projectDescription" style="overflow:hidden;">
+                                <div class="title">Adrenaline rush<br>
+                    Full branding<br>
                     project. </div>
                     <div class="intro-text">
                         <p>
@@ -28,53 +28,75 @@ El camino de modernización de una marca de tradición y dos claves fundamentale
                     </div>
           </div>
           <div class="col-md-6 col-12 p-0">
-            <div class="height30 projectContainer col-12 left ">
-              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline1.png" />
-            </div>      
-            <div class="height35 projectContainer col-12 left">
-              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner fixCentradoVertx2" src="assets/img/portfolio/adrenaline/adrenaline2.png" />
+            <div class="height35 projectContainer d-block d-md-none col-12 left">
+              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline2.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/S6p6TxLTNy8?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>
+    
+            <div class="height30 projectContainer col-12 left ">
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline1.png">
+            </div>      
+            <div class="height35 projectContainer d-none d-md-block col-12 left">
+              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline2.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/S6p6TxLTNy8?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+              </div>
+            </div>
+            </div>
+
+<div class="height30 projectContainer d-block d-md-none col-12 left">
+              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline3.png">
+            </div>
+            <div class="youtube-video-container" style="display:none">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe src="https://www.youtube-nocookie.com/embed/RX95265I4RM?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>
             <div class="height35 projectContainer col-12 left ">
-              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline5.png" />
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline5.png">
             </div>
         </div>
         <div class="col-md-6 col-12 p-0">    
-            <div class="height30 projectContainer col-12 left">
-              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-              <img class="projectInner fixCentradoVertx2" src="assets/img/portfolio/adrenaline/adrenaline3.png" />
+            <div class="height30 projectContainer d-none d-md-block col-12 left">
+              <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+              <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline3.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/RX95265I4RM?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
             </div>
           <div class="projectContainer height35 d-none d-md-block col-md-6 left projectDescription">
-                                <div class="title">Adrenaline rush
-                    Full branding
+                                <div class="title" style="margin-top:5%;">Adrenaline rush<br>
+                    Full branding<br>
                     project. </div>
-                    <div class="intro-text">
-                        <p>
+                    <div class="intro-text" style="height:60%;">
+                        <p style="margin-bottom:0;">
 El camino de modernización de una marca de tradición y dos claves fundamentales para lograrlo. Innovaciones en lanzamiento de productos o presentaciones y un nuevo tono fresco y moderno para su comunicación.</p>
                     </div>
           </div>
-          <div class="height35 projectContainer col-md-6 col-12 left">
-            <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline6.png" />
+          <div class="height35 projectContainer d-none d-md-block col-md-6 col-12 left">
+            <img class="projectInner" src="assets/img/portfolio/adrenaline/adrenaline6.png">
           </div>
           <div class="height35 projectContainer col-12 right ">
-            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'">  
-            <img class="projectInner " src="assets/img/portfolio/adrenaline/adrenaline4.png" />
+            <div onclick="this.nextElementSibling.style.display='block'; this.style.display='none'" class="projectInner">  
+            <img class="projectInner " src="assets/img/portfolio/adrenaline/adrenaline4.png">
             </div>
-            <div class='youtube-video-container' style="display:none">
+            <div class="youtube-video-container" style="display:none">
               <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube-nocookie.com/embed/W1GRXpGa59I?&autoplay=1&mute=1" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe src="https://www.youtube-nocookie.com/embed/BlqXPCh0DHI?&amp;autoplay=0&amp;mute=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
               </div>
             </div>
           </div>
