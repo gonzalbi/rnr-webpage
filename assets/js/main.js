83d0358a7e5c8a7141d5199cef7ea7237fd33cdd
gonzalbi <gonza-martin@hotmail.com>
2021-06-20 21:13:19 -0300

    change prefix fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -247,8 +247,8 @@ function changePrefix(){
   let width = screen.availWidth
   let height= screen.availHeight
   if(onMac){
-    width = window.innerWidth > window.innerHeight ? screen.availHeight : screen.availWidth
-    height = window.innerHeight > window.innerWidth ? screen.availHeight : screen.availWidth
+    width = window.innerWidth > window.innerHeight ? screen.availWidth : screen.availHeight 
+    height = window.innerHeight > window.innerWidth ? screen.availWidth : screen.availHeight 
   }
 
   let portrait = (height > width && width >= 768 && width <= 1200)
